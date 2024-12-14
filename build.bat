c603aacb78ee8f46d751e0abf1bd518d7ff5cd5a
Sasha7b9Work <Sasha7b9@tut.by>
2024-12-14 14:48:29 +0300

    *

--- build.bat
+++ build.bat
@@ -1,1 +1,3 @@
 cmake.exe -B build_vs -G "Visual Studio 17" -A x64 -D URHO3D_OPENGL=ON -D URHO3D_D3D11=ON
+
+MSBuild.exe build_vs/Urho3D.sln /p:Configuration=release /t:build -clp:ErrorsOnly;WarningsOnly -nologo /m
